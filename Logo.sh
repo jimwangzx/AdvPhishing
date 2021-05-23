692e31a652d51fedbaf73c73f67c8325e8c92a59
Termux Official R1 <65804353+termuxhacking-R1@users.noreply.github.com>
2021-05-23 07:40:44 +0000

    Add files via upload

---
+++ Logo.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#clolors
+white='\e[1;37m'
+green='\e[0;32m'
+blue='\e[1;34m'
+red='\e[1;31m'
+yellow='\e[1;33m' 
+echo ""
+echo""
+banner() {
+ echo -e $'\e[1;33m\e[0m\e[1;37m     ███████ ██████  ██    ██    \e[0m''\e[1;33m\e[0m\e[0;31m ██████  ██   ██ ██ ███████   \e[0m'
+ echo -e $'\e[1;33m\e[0m\e[1;37m     ██   ██ ██   █  ██    ██    \e[0m''\e[1;33m\e[0m\e[0;31m ██   █  ██   ██ ██ ██        \e[0m'
+ echo -e $'\e[1;33m\e[0m\e[1;37m     ███████ ██   ██ ██    ██ ███\e[0m''\e[1;33m\e[0m\e[0;31m ██████  ███████ ██ ███████   \e[0m'
+ echo -e $'\e[1;33m\e[0m\e[1;37m     ██   ██ ██   ██  ██  ██     \e[0m''\e[1;33m\e[0m\e[0;31m ██      ██   ██ ██      ██   \e[0m'
+ echo -e $'\e[1;33m\e[0m\e[1;37m     ██   ██ ██████    ████      \e[0m''\e[1;33m\e[0m\e[0;31m ██      ██   ██ ██ ███████   \e[0m'
+  echo""    
+  echo -e $'\e[1;31m\e[0m\e[1;31m    ██████████\e[0m''\e[1;37m██████████''\e[1;32m\e[0m\e[1;32m██████████\e[0m' '\e[1;32m\e[0m\e[1;37m OTP BYPASS PHISHING TOOL \e[0m'                                       
+  echo ""
+   echo -e $'\e[1;33m\e[0m\e[1;33m  [ \e[0m\e[1;33m Follow on Github :- https://github.com/termuxhacking-R1/Advphishing \e[0m \e[1;32m\e[0m\e[1;33m] \e[0m'
+   echo ""
+      echo -e $'\e[1;37m\e[0m\e[1;37m   +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+ >>\e[0m'
+      echo -e "\e[93m   |A|D|V|A|N|C|E| |A|D|V| |P|H|I|S|H|I|N|G| |T|O|O|L|"      
+      echo -e $'\e[1;37m\e[0m\e[1;37m   +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+ >>\e[0m' 
+      echo ""                                                
+      }
+      banner
